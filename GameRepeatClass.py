337f00eaefc9ff20530a9945ce34c7b6f4bf68c9
Andrey Gorlov <andreygorlov19995@gmail.com>
2021-04-16 23:28:50 +0300

    Исправлены ошибки

--- GameRepeatClass.py
+++ GameRepeatClass.py
@@ -21,9 +21,9 @@ class GameRepeatClass(tk.Canvas):
                 self.__map[row].append(TileClass(-1, image, bgImage, self.__size * col, self.__size * row, self.__size, self))
         for row in range(8):
             for col in range(8):
-                buf = randint(0, 32)
-                while not self.findRepeat(buf):
-                    buf = randint(0, 32)
+                buf = randint(0, 31)
+                while not self.__findRepeat(buf):
+                    buf = randint(0, 31)
                 self.__map[row][col] = TileClass(buf, image, bgImage, self.__size * col+25, self.__size * row+25, self.__size, self)
 
         self.draw()
@@ -31,23 +31,27 @@ class GameRepeatClass(tk.Canvas):
     def leftClick(self, event):
         col = event.x // self.__size
         row = event.y // self.__size
-        self.__map[row][col].click()
-        print(self.__map[row][col].number)
-        self.draw()
-        self.update()
-        self.__count += 1
-        if self.__count == 1:
-            self.__buf = self.__map[row][col]
-        elif self.__buf.number == self.__map[row][col].number:
-            time.sleep(0.5)
-            self.__buf.number = -1
-            self.__map[row][col].number = -1
-        else:
-            time.sleep(0.5)
-            self.__buf.status = False
-            self.__map[row][col].status = False
-            self.__count = 0
-        self.draw()
+        if not self.__map[row][col].status:
+            self.__map[row][col].click()
+            print(self.__map[row][col].number)
+            self.draw()
+            self.update()
+            self.__count += 1
+            if self.__count == 1:
+                self.__buf = self.__map[row][col]
+            elif self.__buf.number == self.__map[row][col].number:
+                self.__count = 0
+                time.sleep(0.5)
+                self.__buf.number = -1
+                self.__map[row][col].number = -1
+            else:
+                time.sleep(0.5)
+                self.__buf.status = False
+                self.__map[row][col].status = False
+                self.__count = 0
+            self.draw()
+        #if not self.__win():
+
 
     def draw(self):
         self.delete("all")
@@ -55,8 +59,9 @@ class GameRepeatClass(tk.Canvas):
             for j in range(8):
                 self.create_rectangle(self.__size * j, self.__size * i, self.__size * (j + 1), self.__size * (i + 1))
                 self.__map[i][j].draw()
+                #self.create_text(self.__size * j + 10, self.__size * i + 10, text=self.__map[i][j].number)
 
-    def findRepeat(self, b):
+    def __findRepeat(self, b):
         count = 0
         for i in range(8):
             for j in range(8):
@@ -68,5 +73,10 @@ class GameRepeatClass(tk.Canvas):
                     return True
         return True
 
-
+    def __win(self):
+        for i in range(8):
+            for j in range(8):
+                if self.__map[i][j].number != -1:
+                    return False
+        return True
 
--- main.py
+++ main.py
@@ -25,6 +25,10 @@ def loadImage():
         imageArr.append(pi)
 
 def StartGameRepeat():
+    delButton()
+
+
+def StartGameSearch():
     loadImage()
     global imageArr
     pi = Image.open("resource/field.jpg")
@@ -32,10 +36,6 @@ def StartGameRepeat():
     pi = PhotoImage(pi)
     game = GameRepeatClass(imageArr, pi, bg='#FF00FF', highlightbackground="#8B4513")
     game.place(x=100, y=100, width=400, height=400)
-    delButton()
-
-
-def StartGameSearch():
     delButton()
 
 
@@ -48,9 +48,9 @@ imag = img.resize((600, 600), Image.ANTIALIAS)
 image = ImageTk.PhotoImage(imag)
 panel = tk.Label(root, image=image)
 panel.pack(side="top", fill="both", expand="no")
-buttonRepeatGame = tk.Button(root, text='Start Game Repeat', command=StartGameRepeat, bg='#DEB887')
+buttonRepeatGame = tk.Button(root, text='Start Game Search', command=StartGameSearch, bg='#DEB887')
 buttonRepeatGame.place(x=200, y=200, width=200)
-buttonSearchGame = tk.Button(root, text='Start Game Search', command=StartGameSearch, bg='#DEB887')
+buttonSearchGame = tk.Button(root, text='Start Game Repeat', command=StartGameRepeat, bg='#DEB887')
 buttonSearchGame.place(x=200, y=300, width=200)
 buttonQuit = tk.Button(root, text='Quit', command=root.quit, bg='#DEB887')
 buttonQuit.place(x=200, y=400, width=200)
